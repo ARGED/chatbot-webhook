2070863970a0b0207b9a1f3db6dc14b09e471ca3
Erik Bauer <erik_bauer@hotmail.com>
2019-01-30 12:24:18 +0100

    Changed the expected json format

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ server.use(bodyParser.json());
 
 server.post('/get-customer-data', (req, res) => {
 
-  const dataToGet = req.body.result && req.body.result.parameters && req.body.result.parameters.energy ? req.body.result.parameters.energy : 'Error';
+  const dataToGet = req.body.queryResult && req.body.queryResult.parameters && req.body.queryResult.parameters.energy ? req.body.queryResult.parameters.energy : 'Error';
   const reqUrl = `https://api-kth.azurewebsites.net/api/${dataToGet}?id=00002&code=${API_KEY}`;
   https.get(reqUrl, (responseFromAPI) => {
     let completeResponse = '';
